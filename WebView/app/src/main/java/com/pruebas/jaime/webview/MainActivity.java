db7c0a10df0185097fd0d8d32fe17fc59cfe5451
jaimelombanag <alombana24@hotmail.com>
2018-09-19 12:29:50 -0500

    Se sube el programa falta iconos

---
+++ WebView/app/src/main/java/com/pruebas/jaime/webview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.pruebas.jaime.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://www.ontabebe.shop");
+    }
+}
