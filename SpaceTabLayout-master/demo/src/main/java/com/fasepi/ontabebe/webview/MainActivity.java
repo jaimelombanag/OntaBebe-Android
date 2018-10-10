b36ab7e07c22a5db312f0b0c6c94e77af69a7279
jaimelombanag <alombana24@hotmail.com>
2018-10-10 16:53:34 -0500

    Se colocan permisos de camara

--- SpaceTabLayout-master/demo/src/main/java/com/fasepi/ontabebe/webview/MainActivity.java
+++ SpaceTabLayout-master/demo/src/main/java/com/fasepi/ontabebe/webview/MainActivity.java
@@ -1,11 +1,17 @@
 package com.fasepi.ontabebe.webview;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -17,6 +23,7 @@ import eu.long1.spacetablayout.SpaceTabLayout;
 
 public class MainActivity extends AppCompatActivity {
     SpaceTabLayout tabLayout;
+    public static final int REQUEST_STORAGE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,19 @@ public class MainActivity extends AppCompatActivity {
         fragmentList.add(new FragmentC());
         fragmentList.add(new FragmentD());
         //fragmentList.add(new FragmentE());
+
+
+
+        String[] PERMISSIONS = {android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.CAMERA};
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!hasPermissions(this, PERMISSIONS)) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_STORAGE);
+            } else {
+                Log.e("TAG", "Ya tiene los permisos");
+            }
+        }
+
+
 
         final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.activity_main);
 
@@ -54,5 +74,34 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplication(), "" + tabLayout.getCurrentPosition(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
+    private static boolean hasPermissions(Context context, String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /*get Permissions Result*/
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.e("TAG", "PERMISSION_GRANTED");
+                } else {
+                    Log.e("TAG", "The app was not allowed to write to your storage");
+                }
+            }
+
+        }
     }
 }
